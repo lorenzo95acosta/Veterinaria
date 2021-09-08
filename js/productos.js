47d9f7d45f6a9c483f833cd822de5210f8db57a2
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2021-09-08 19:43:12 -0300

    Agrego nuestro JSON

--- js/productos.js
+++ js/productos.js
@@ -3,7 +3,7 @@ let listaProductos=[];
 function mostrarProductos(){
     let html="";
     for(producto of listaProductos){
-        html += `<h3>${producto.nombre}</h3>
+        html += `<h3>${producto.name}</h3>
         <li>${producto.descripcion}</li>
         <li>${producto.precio}</li>`
     }
@@ -12,7 +12,7 @@ function mostrarProductos(){
 }
 
 document.addEventListener("DOMContentLoaded",function(e){
-    peticion("https://api.npoint.io/89dfbc7824a6ef5ba341")
+    peticion("https://gist.githubusercontent.com/anaclaraolivera/ea778a89ae96a339c88b8ca265805976/raw/0bb5bc8eae260acc5ccc493f6e51f8eb578b1ecc/gistfile1.txt")
     .then(respuesta=>{
         if(respuesta.estado==="ok"){
             listaProductos= respuesta.datos;
