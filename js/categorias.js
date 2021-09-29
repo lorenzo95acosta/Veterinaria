9fb2e57edffaa8cdda7e3445e237d9d94076a995
anaclaraolivera <acolivera13@gmail.com>
2021-09-29 19:05:02 -0300

    cambiamos de lugar una función

--- js/categorias.js
+++ js/categorias.js
@@ -1,10 +1,10 @@
-function saludar(){
-    let string = ', ';
-    string += localStorage.getItem('nombre');
-    let nombreUsuario = document.getElementById('nombreUsuario');
-    nombreUsuario.innerHTML += string;
-}
+// function saludar(){
+//     let string = ', ';
+//     string += localStorage.getItem('nombre');
+//     let nombreUsuario = document.getElementById('nombreUsuario');
+//     nombreUsuario.innerHTML += string;
+// }
 
 document.addEventListener("DOMContentLoaded",function(e){
-    saludar()
+    // saludar()
 })
