0dbed94914e63cd33f8a840f5ce166a390000121
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2021-10-04 21:10:37 -0300

    Carrito-ejemplo
    Agrego archivos del ejemplo de carrito

---
+++ js/carrito-ej.js
@@ -0,0 +1,18 @@
+//url para carrito https://virmorrone.github.io/veterinaria-api/carrito-final.json
+
+document.getElementById("1").addEventListener("change",function(e){
+    let cantidad = document.getElementById("1").value;
+    console.log(cantidad);
+    
+})
+
+function update(){
+
+}
+
+function showCarrito(){
+
+    /*mostrar los productos del carrito con el input correspondiente a la cantidad*/
+
+}
+
